eb56c90e5fc53f094d7e3f513d9fc4bbcb3171c5
Félix Saparelli <felix@passcod.name>
2018-05-27 18:33:04 +1200

    Allow custom stream to be given to logger

--- test/logger.js
+++ test/logger.js
@@ -1,4 +1,5 @@
 const { test } = require('tap')
+const { WritableMock } = require('stream-mock')
 const logger = require('../lib/logger')
 
 test('provides $levels', (t) => {
@@ -11,6 +12,38 @@ test('provides $levels', (t) => {
   t.type(log.$error, Function)
   t.type(log.$fatal, Function)
   t.done()
+})
+
+test('formats object inputs', (t) => {
+  const stream = new WritableMock()
+  const log = logger('test', stream)
+
+  stream.on('finish', () => {
+    t.match(
+      encodeURIComponent(stream.data),
+      '%5Btest%5D%20%E2%80%BA%20error%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%7B%20foo%3A%20123%20%7D%0A'
+    )
+    t.done()
+  })
+
+  log.$error({ foo: 123 })
+  stream.end()
+})
+
+test('formats Error inputs', (t) => {
+  const stream = new WritableMock()
+  const log = logger('test', stream)
+
+  stream.on('finish', () => {
+    t.match(
+      encodeURIComponent(stream.data.toString().split('\n')[0]),
+      '%5Btest%5D%20%E2%80%BA%20error%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20Error%3A%20baz%20'
+    )
+    t.done()
+  })
+
+  log.$error(new Error('baz'))
+  stream.end()
 })
 
 test('$ shorthand returns first argument', (t) => {
