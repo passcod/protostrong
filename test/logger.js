463d8f103cfb16148ea86b6844752288f22655c6
Félix Saparelli <felix@passcod.name>
2018-05-27 14:54:02 +1200

    Adjust logger levels

--- test/logger.js
+++ test/logger.js
@@ -1,19 +1,19 @@
 const { test } = require('tap')
 const logger = require('../lib/logger')
 
-test('provides $aliases', (t) => {
+test('provides $levels', (t) => {
   const log = logger('test')
 
+  t.type(log.$trace, Function)
   t.type(log.$debug, Function)
   t.type(log.$info, Function)
-  t.type(log.$log, Function)
   t.type(log.$warn, Function)
   t.type(log.$error, Function)
   t.type(log.$fatal, Function)
   t.done()
 })
 
-test('$ alias returns first argument', (t) => {
+test('$ shorthand returns first argument', (t) => {
   const { $ } = logger('test')
 
   t.type($, Function)
